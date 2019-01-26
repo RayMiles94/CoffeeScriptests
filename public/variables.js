53c762517e31e41285d59f1dba5be09fe37eb1c8
RayMiles94 <natsuboy94@gmail.com>
2019-01-26 19:30:11 +0100

    add variables js

---
+++ public/variables.js
@@ -0,0 +1,15 @@
+var a, age, b, c, d, name;
+
+name = "Javed";
+
+age = 25;
+
+a = 5;
+
+b = 6;
+
+c = a + b;
+
+d = true;
+
+d = a === b;
---
+++ src/variables.coffee
@@ -0,0 +1,7 @@
+name = "Javed"
+age = 25
+a = 5 
+b = 6
+c = a + b
+d = true
+d = a == b 
