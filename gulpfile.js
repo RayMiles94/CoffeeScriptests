0dab7ecc287e85e26c40fc76e3d52c7d9a4892c7
RayMiles94 <natsuboy94@gmail.com>
2019-01-26 19:14:08 +0100

    add first coffeescript file

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var coffee = require('gulp-coffee');
+ 
+gulp.task('coffee', function() {
+  gulp.src('./src/*.coffee')
+    .pipe(coffee({bare: true}))
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('default',['coffee']);
