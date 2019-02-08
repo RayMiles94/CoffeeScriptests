0d5a0357bac7153b98d5d6519604113ab9682d3b
RayMiles94 <natsuboy94@gmail.com>
2019-02-08 15:34:04 +0100

    change in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,8 @@
 var gulp   = require('gulp');
 var coffee = require('gulp-coffee');
 
- 
 gulp.task('coffee', function() {
-  gulp.src('./src/*.coffee')
-    .pipe(coffee({bare: true}))
-    .pipe(gulp.dest('./public/'));
+  gulp.src('./src/*.coffee').pipe(coffee({bare: true})).pipe(gulp.dest('./public/'));
 });
 
 gulp.task('default',['coffee']);
